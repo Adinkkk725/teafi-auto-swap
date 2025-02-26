568ad38d0b8ad71e1c110f30c3915c4ff36fb1eb
Adinkkk725 <121096995+Adinkkk725@users.noreply.github.com>
2025-02-26 22:45:37 +0800

    Update swap.js

--- swap.js
+++ swap.js
@@ -1,19 +1,33 @@
 const axios = require('axios');
 
-async function swapTokens(amount, fromToken, toToken) {
-    const privateKey = process.env.PRIVATE_KEY;
+async function swapTokens(amount, fromToken, toToken, fromTokenSymbol, toTokenSymbol, walletAddress, hash, blockchainId, gasFeeTokenAddress, gasFeeTokenSymbol, gasFeeAmount) {
     const url = process.env.SWAP_URL;
+
+    const data = {
+        hash,
+        blockchainId,
+        type: 3,
+        walletAddress,
+        fromTokenAddress: fromToken,
+        toTokenAddress: toToken,
+        fromTokenSymbol,
+        toTokenSymbol,
+        fromAmount: amount,
+        toAmount: amount,
+        gasFeeTokenAddress,
+        gasFeeTokenSymbol,
+        gasFeeAmount
+    };
 
     try {
         // Panggil API untuk melakukan swap
-        const response = await axios.post(url, {
-            privateKey,
-            fromToken,
-            toToken,
-            amount
-        }, {
+        const response = await axios.post(url, data, {
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Accept': 'application/json, text/plain, */*',
+                'Origin': 'https://app.tea-fi.com',
+                'Referer': 'https://app.tea-fi.com/',
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36'
             }
         });
         console.log(`Swap berhasil: ${response.data}`);
